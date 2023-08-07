24581f5010b23e6aff348b22b10f7c2ac55d497e
Nguyen Sy Nam <namnguyen4@cqtdt.asia>
2023-08-07 10:04:57 +0700

    config log

--- parameterdownload/src/main/resources/start.sh
+++ parameterdownload/src/main/resources/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup java -Dlogback.configurationFile=/home/eco1056-namns/Workspace/job/parameterdownload/src/main/resources/log4j2.xml -Dfile.ending=UTF8 -Dspring.config.location=/home/eco1056-namns/Workspace/job/parameterdownload/src/main/resources/application.yml -jar /home/eco1056-namns/Workspace/job/parameterdownload/target/parameterdownload-0.0.1-SNAPSHOT.jar  &
-tail -f /home/eco1056-namns/Desktop/file/logs/parameterDownload.log
+nohup java -Dlogback.configurationFile=/home/eco1056-namns/Workspace/job/autoDownload/parameterdownload/src/main/resources/log4j2.xml -Dfile.ending=UTF8 -Dspring.config.location=/home/eco1056-namns/Workspace/job/autoDownload/parameterdownload/src/main/resources/application.yml -jar /home/eco1056-namns/Workspace/job/autoDownload/parameterdownload/target/parameterdownload-0.0.1-SNAPSHOT.jar  &
+tail -f /home/eco1056-namns/Desktop/file/logs/spring.log
--- parameterdownload/src/main/resources/stop.sh
+++ parameterdownload/src/main/resources/stop.sh
@@ -1,1 +1,1 @@
-pkill -f /home/eco1056-namns/Workspace/job/parameterdownload/target/parameterdownload-0.0.1-SNAPSHOT.jar
+pkill -f /home/eco1056-namns/Workspace/job/autoDownload/parameterdownload/target/parameterdownload-0.0.1-SNAPSHOT.jar
